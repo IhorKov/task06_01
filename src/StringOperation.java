7950af20ef860b851113e5e26f3ffa49a4909bdc
IhorKov <apasniy1234@gmail.com>
2021-06-14 12:22:15 +0300

    add buffer

--- src/StringOperation.java
+++ src/StringOperation.java
@@ -1,5 +1,10 @@
-public class StringOperation {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class StringOperation {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = reader.readLine();
     }
 }
